3f505e632083264412b21ef7eca8f8cf0caaff62
aguaiyoung <aguaiyoung@gmail.com>
2017-12-17 19:29:20 -0800

    updated wiki

---
+++ wiki/demo/js/weddingplaylist.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+   {
+        mp3:'http://qqma.tingge123.com:823/mp3/2015-05-09/1431181573.mp3',
+        title:'20180101 Wedding',
+        artist:'Zhou chao && Xiong bingqing',
+        rating:5,
+        buy:'#',
+        duration:'4:3',
+        cover:'mix/zc_2.png'
+    }
+];
